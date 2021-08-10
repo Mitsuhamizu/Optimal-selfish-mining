03679bef6c4ae2f2e5ccf6f29d497a4953177b28
Zhichun Lu <luzhic01@gmail.com>
2021-08-10 17:23:04 +0800

    feat: generate rewar metrix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 IRRELEVANT, RELEVANT, ACTIVE = 0, 1, 2
 
-ADOPT, OVERRIDE, WATI, MATCH = 0, 1, 2, 3
+ADOPT, OVERRIDE, WAIT, MATCH = 0, 1, 2, 3
 
 
 def get_index(a, h, f, rounds, fork_states_num):
@@ -11,9 +11,9 @@ def get_index(a, h, f, rounds, fork_states_num):
     return a * rounds * fork_states_num + h * fork_states_num + f
 
 
-def generate_probability_matrix(states_num, action_num, rounds, fork_states_num):
+def generate_probability_matrix(states_num, action_num, rounds, fork_states_num, gamma):
 
-    P = np.zeros([states_num, states_num, action_num])
+    P = np.zeros([action_num, states_num, states_num])
     # the structure of probability is (A, H, F)
     # irrelevant = 0, relevant =1, active = 2
     # (0, 0, 0)
@@ -31,13 +31,13 @@ def generate_probability_matrix(states_num, action_num, rounds, fork_states_num)
     adversary_height, honest_height = 1, 0
     index_column_current = get_index(adversary_height, honest_height,
                                      IRRELEVANT, rounds, fork_states_num)
-    P[:, index_column_current, ADOPT] = alpha
+    P[ADOPT, :, index_column_current] = alpha
     # with probablity 1 - alpha to (0, 1, IRRELEVANT) I am not sure if it should be (0, 1, RELEVANT)
     # 可能是错的。
     adversary_height, honest_height = 0, 1
     index_column_current = get_index(adversary_height, honest_height,
                                      IRRELEVANT, rounds, fork_states_num)
-    P[:, index_column_current, ADOPT] = 1-alpha
+    P[ADOPT, :, index_column_current] = 1-alpha
 
     # probability under action override.
     # only works for a > h.
@@ -53,14 +53,14 @@ def generate_probability_matrix(states_num, action_num, rounds, fork_states_num)
                 # with probablity alpha to ((a - h), 0, 0).
                 index_column_current = get_index(
                     (a-h), 0, IRRELEVANT, rounds, fork_states_num)
-                P[index_row_begin:index_row_end+1,
-                    index_column_current, OVERRIDE] = alpha
+                P[OVERRIDE, index_row_begin:index_row_end+1,
+                    index_column_current] = alpha
 
                 # with probablity alpha to ((a - h - 1), 1, 1).
                 index_column_current = get_index(
                     (a-h-1), 1, RELEVANT, rounds, fork_states_num)
-                P[index_row_begin:index_row_end+1,
-                    index_column_current, OVERRIDE] = 1-alpha
+                P[OVERRIDE, index_row_begin:index_row_end+1,
+                    index_column_current] = 1-alpha
 
     # probability under action wait.
     for a in range(0, rounds-1):
@@ -68,27 +68,27 @@ def generate_probability_matrix(states_num, action_num, rounds, fork_states_num)
             # IRRELEVANT
             index_row = get_index(
                 a, h, IRRELEVANT, rounds, fork_states_num)
-            P[index_row, get_index(
-                a+1, h, IRRELEVANT, rounds, fork_states_num), WATI] = alpha
-            P[index_row, get_index(
-                a, h+1, RELEVANT, rounds, fork_states_num), WATI] = 1-alpha
+            P[WAIT, index_row, get_index(
+                a+1, h, IRRELEVANT, rounds, fork_states_num)] = alpha
+            P[WAIT, index_row, get_index(
+                a, h+1, RELEVANT, rounds, fork_states_num)] = 1-alpha
 
             # RELEVANT
             index_row += 1
             P[index_row, get_index(
-                a+1, h, IRRELEVANT, rounds, fork_states_num), WATI] = alpha
+                a+1, h, IRRELEVANT, rounds, fork_states_num), WAIT] = alpha
             P[index_row, get_index(
-                a, h+1, RELEVANT, rounds, fork_states_num), WATI] = 1-alpha
+                a, h+1, RELEVANT, rounds, fork_states_num), WAIT] = 1-alpha
 
             # ACTIVE
             index_row += 1
             P[index_row, get_index(
-                a+1, h, ACTIVE, rounds, fork_states_num), WATI] = alpha
+                a+1, h, ACTIVE, rounds, fork_states_num), WAIT] = alpha
             #  这里错了，要注意。
             P[index_row, get_index(
-                a-h, 1, RELEVANT, rounds, fork_states_num), WATI] = gamma*(1-alpha)
+                a-h, 1, RELEVANT, rounds, fork_states_num), WAIT] = gamma*(1-alpha)
             P[index_row, get_index(
-                a, h+1, RELEVANT, rounds, fork_states_num), WATI] = (1-gamma)*(1-alpha)
+                a, h+1, RELEVANT, rounds, fork_states_num), WAIT] = (1-gamma)*(1-alpha)
 
     # probability under action match.
     for a in range(0, rounds-1):
@@ -106,17 +106,46 @@ def generate_probability_matrix(states_num, action_num, rounds, fork_states_num)
 
 
 def generate_reward_matrix(states_num, action_num, rounds, fork_states_num, rho):
-    R = np.zeros([states_num, states_num, action_num])
+    R = np.zeros([states_num, action_num])
     # reward under action adopt.
-    
+    for a in range(0, rounds):
+        for h in range(0, rounds):
+            index_row_begin = get_index(
+                a, h, IRRELEVANT, rounds, fork_states_num)
+            index_row_end = index_row_begin+2
+            R[index_row_begin:index_row_end+1, ADOPT] = -rho * h
+            R[index_row_begin:index_row_end+1, ADOPT] = -rho * h
     # reward under action override.
+    for a in range(0, rounds-1):
+        for h in range(0, rounds-1):
+            if a > h:
+                index_row_begin = get_index(
+                    a, h, IRRELEVANT, rounds, fork_states_num)
+                index_row_end = index_row_begin+2
+
+                R[index_row_begin:index_row_end+1, OVERRIDE] = (1-rho) * (h+1)
+                R[index_row_begin:index_row_end+1, OVERRIDE] = (1-rho) * (h+1)
     # reward under action wait.
+    for a in range(0, rounds-1):
+        for h in range(0, rounds-1):
+            # ACTIVE
+            index_row = get_index(
+                a, h, ACTIVE, rounds, fork_states_num)
+            R[index_row, WAIT] = (1-rho)*h
+
     # reward under action match.
+    for a in range(0, rounds-1):
+        for h in range(0, rounds-1):
+            index_row = get_index(
+                a, h, RELEVANT, rounds, fork_states_num)
+            R[index_row, MATCH] = (1-rho)*h
+    return R
 
 
 if __name__ == "__main__":
     low, high, epsilon = 0, 1, pow(10, -5)
     rounds = 76
+    max_iter = 1000
     # There are three different fork for the sanme height combination.
     states_num = rounds*rounds*3
     action_num = 4
@@ -127,21 +156,17 @@ if __name__ == "__main__":
     # four actions: adopt, override, wait, match.
 
     P = generate_probability_matrix(
-        states_num, action_num, rounds, fork_states_num)
+        states_num, action_num, rounds, fork_states_num, gamma)
     R = generate_reward_matrix(
-        states_num, action_num, rounds, fork_states_num)
+        states_num, action_num, rounds, fork_states_num, rho)
+    print(P.shape)
+    print(R.shape)
+    # rvi = mdptoolbox.mdp.RelativeValueIteration(P, R, max_iter=max_iter)
+    # rvi.run()
+    # print(rvi.policy)
     # while high-low > epsilon:
     #     rho = (low+high)/2
 
     #     # generate Reward with different rho.
 
-    # rewaird under action adopt.
-    # rewaird under action override.
-    # rewaird under action wait.
-    # rewaird under action match.
-    #     R = np.array([[5, 10], [-1, 2]])
-
-    #     max_iter = 1000
-    #     vi = mdptoolbox.mdp.RelativeValueIteration(P, R, max_iter=max_iter)
-    #     vi.run()
     #     print(vi.policy)
--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-result = []
-for a in range(0, 76):
-    for h in range(0, 76):
-        for f in range(0, 3):
-            result.append((a, h, f))
+import mdptoolbox.example
 
-a = 1
-h = 0
-f = 0
-index = a * 3 * 76 + h * 3 + f
-print(index)
-print(a, h, f)
-print(result[index])
+P, R = mdptoolbox.example.forest()
+print(P.shape)
+print(R.shape)
