5fa5be91d9bf15ffebb8086e05859cb1b4101775
Zhichun Lu <luzhic01@gmail.com>
2021-08-16 20:13:33 +0800

    chore: with debug

--- main.py
+++ main.py
@@ -318,7 +318,13 @@ def generate_reward_matrix(states_num, action_num, rounds, fork_states_num, alph
     for action in [OVERRIDE, WAIT, MATCH]:
         R[action] = adopt_reward(
             R[action], rounds, fork_states_num, alpha, rho, pay_type)
-
+    # for action in [ADOPT, OVERRIDE, WAIT, MATCH]:
+    #     column_names = [get_state(index, rounds, fork_states_num)
+    #                     for index in range(0, states_num)]
+    #     row_name = [get_state(index, rounds, fork_states_num)
+    #                 for index in range(0, states_num)]
+    #     df = pd.DataFrame(R[action], columns=column_names, index=row_name)
+    #     df.to_csv('R-{}.csv'.format(action), sep='\t')
     R = [sparse(R[ADOPT]), sparse(R[OVERRIDE]),
          sparse(R[WAIT]), sparse(R[MATCH])]
     return R
@@ -327,7 +333,7 @@ def generate_reward_matrix(states_num, action_num, rounds, fork_states_num, alph
 if __name__ == "__main__":
     starttime = datetime.datetime.now()
     low, high, epsilon = 0, 1, pow(10, -5)
-    rounds = 20
+    rounds = 4
 
     # There are three different fork for the sanme height combination.
     states_num = rounds*rounds*3
